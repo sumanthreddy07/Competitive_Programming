6962f8eee33dd8a4044a0b260a2243f9f250eb4d
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2020-07-08 11:15:46 +0530

    rename files using rename_files.py

---
+++ Python/rename_files.py
@@ -0,0 +1,14 @@
+import os
+import re
+
+path = "/home/sumanth/Coding/GIT/Competitive_Programming/CPP/July/Week_1"
+
+for file in os.listdir(path):
+
+    file_without_ext = os.path.splitext(file)[0]
+    extension = os.path.splitext(file)[1]
+    
+    name = re.split(r"[()]", file_without_ext)
+    new_file_name = name[1] + "(" + name[0] + ")" + extension
+    os.rename(os.path.join(path,file),os.path.join(path,new_file_name))
+    print(new_file_name)
