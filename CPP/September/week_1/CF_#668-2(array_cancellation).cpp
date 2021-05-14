33a35250ff47b1490f79d13077b50d576ba79286
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2021-05-14 13:44:16 +0530

    upload code

---
+++ CPP/September/week_1/CF_#668-2(array_cancellation).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <iomanip>
+//#include <math.h>
+//#include <unistd.h> //for sleep
+//#include <limits.h>
+//#include <map> <unordered_map>
+//#include <set> 
+
+#define endl "\n"
+#define run(i,a,b) for(long i=a;i<=b;i++)
+#define revrun(i,a,b) for(long i=b;i>=a;i--)
+//#define PAIRLL pair<long long,long long>
+//#define PAIRII pair<int,int>
+//#define pi 3.14159265358979323846
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define LL long long
+#define L long
+const LL mod=1000000007;
+
+using namespace std;
+
+void solve()
+{
+    L n;
+    cin>>n;
+
+    vector<LL>A(n);
+
+    run(i,0,n-1)
+        cin>>A[i];
+    revrun(i,1,n-1)
+    {
+        if(A[i]<0)
+        {
+            A[i-1]+=A[i];
+        }
+    }
+    
+    cout<<abs(A[0])<<endl;
+}
+int main()
+{
+    IOS;
+    L t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
